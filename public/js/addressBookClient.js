cd20bf3de1c61ebbdbba78d00819e5cea84f6108
Oliver Sun <olisun@mac.com>
2019-12-28 13:35:50 -0800

    refactored code in addressBook.js to append data from mySQL to the DOM

--- public/js/addressBookClient.js
+++ public/js/addressBookClient.js
@@ -3,14 +3,15 @@ $(document).ready(function () {
   var name = $("#name");
   var address = $("#address");
   var submit = $("#submit");
+  var submit2 = $("#submit2");
+
+  var displayData = $("#whereDataSpitsOut")
 
   $(document).on("click", "#addData", function (event) {
     event.preventDefault();
     console.log('on-click running')
     addData();
   })
-
-  getContacts();
 
   function addData() {
     // event.preventDefault();
@@ -28,17 +29,53 @@ $(document).ready(function () {
     console.log(userData)
     $.post("/api/contacts", userData)
       .then(getContacts);
-    console.log(userData);
   }
 
   function getContacts() {
+    console.log("inside getContacts")
     $.get("/api/contacts", function (data) {
       name.val("");
       address.val("");
+      console.log(data);
+      for (var i = 0; i < data.length; i++) {
+        console.log(data[i].name);
+        console.log(data[i].address);
+        displayData.append(data[i].name);
+        displayData.append(data[i].address);
+      }
     });
   }
+
+  submit2.on("click", function () {
+    alert("Clicked");
+  });
 
   submit.on("click", function () {
     alert("Thank you! Message sent.")
   });
+
+  function getDataBase() {
+    console.log("inside getDB")
+    $.get("/api/contacts", function (data) {
+      var rowsToAdd = [];
+      for (var i = 0; i < data.length; i++) {
+        rowsToAdd.push(createRow(data[i]));
+      }
+      console.log(data)
+      renderList(rowsToAdd);
+      nameInput.val("");
+    });
+  }
+
+  // function renderList(rows) {
+  //   dataList.children().not(":last").remove();
+  //   dataContainer.children(".alert").remove();
+  //   if (rows.length) {
+  //     console.log(rows);
+  //     dataList.prepend(rows);
+  //   }
+  //   else {
+  //     renderEmpty();
+  //   }
+  // }
 });
