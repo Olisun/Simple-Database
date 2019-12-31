15634029adff4aad614b8d72ec839029172f055b
Oliver Sun <olisun@mac.com>
2019-12-31 14:17:22 -0800

    added code to target data to be updated

--- public/js/addressBookClient.js
+++ public/js/addressBookClient.js
@@ -8,6 +8,8 @@ $(document).ready(function () {
   $(document).on("click", "#addData", addData);
   $(document).on("click", "#addData", showData);
   $(document).on("click", ".delete", deleteContact);
+  $(document).on("click", ".edit", editContact);
+
 
   function addData() {
     event.preventDefault();
@@ -46,7 +48,7 @@ $(document).ready(function () {
         <div class="card-body">
           <h5 class="card-title">${data[i].name}</h5>
           <p class="card-text">${data[i].address}</p>
-          <button class="edit btn btn-primary" data-id=${data[i].id} type="button">Edit</button>
+          <button class="edit btn btn-primary" data-id=${data[i].id} data-name=${data[i].name} data-address=${data[i].address} type="button">Edit</button>
           <button class="delete btn btn-primary" data-id=${data[i].id} type="button">Delete</button>
         </div>
       </div>`)
@@ -66,14 +68,38 @@ $(document).ready(function () {
     }).then(getContacts)
       .then(showData);
   }
+
+  function editContact(data) {
+    // click on the edit button and an input box shows for name and address. 
+    // type in the input boxes and the values are captured. 
+    // make an ajax call to database to update those fields. 
+    // call showData to display on the page. 
+
+    console.log("edit button clicked");
+    console.log(this);
+    var name = $(this).data("name");
+    console.log(name);
+    var address = $(this).data("address");
+    console.log(address);
+    var updateContact = {
+      name: name,
+      address: address
+    };
+    $.ajax({
+      method: "PUT",
+      url: "/api/contacts/",
+      data: updateContact
+    }).then(getContacts)
+      .then(showData);
+  }
 });
 
-function editContact() {
-  var currentContact = $(this).data("name")
-  $(this).children().hide();
-  $(this).children("input").val(currentContact.text);
-  $(this).children("input.edit").show();
-  $(this).children("input.edit").focus();
-}
 
 
+
+
+  // var currentContact = $(this).data("name")
+  // $(this).children().hide();
+  // $(this).children("input").val(currentContact.text);
+  // $(this).children("input.edit").show();
+  // $(this).children("input.edit").focus();
