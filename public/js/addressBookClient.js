234da2b7dd8c70c35d4e54f0ba595f835602d3e4
Oliver Sun <olisun@mac.com>
2020-01-03 10:51:28 -0800

    added a modal pop-up inside each contact card in client js

--- public/js/addressBookClient.js
+++ public/js/addressBookClient.js
@@ -40,16 +40,38 @@ $(document).ready(function () {
   function showData() {
     console.log("inside showDB")
     $.get("/api/contacts", function (data) {
-      var editData = $(".updateData");
       console.log(data);
       displayData.empty();
       for (var i = 0; i < data.length; i++) {
         displayData.append(`<div class="card" style="width: 18rem;">
         <div class="card-body">
           <h5 class="card-title">${data[i].name}</h5>
-          <p class="card-text">${data[i].address}</p>
-          <button class="edit btn btn-primary" data-id="${data[i].id}" data-name="${data[i].name}" data-address="${data[i].address}" type="button">Edit</button>
+          <button class="details btn btn-primary" data-id="${data[i].id}" data-name="${data[i].name}" data-address="${data[i].address}" data-toggle="modal" data-target="#exampleModalCenter" type="button">Details</button>
           <button class="delete btn btn-primary" data-id="${data[i].id}" data-name="${data[i].name}" data-address="${data[i].address}" type="button">Delete</button>
+
+          <!-- Modal -->
+          <div class="modal fade" id="exampleModalCenter"  role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+            <div class="modal-dialog modal-dialog-centered" role="document">
+              <div class="modal-content">
+                <div class="modal-header">
+                  <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                  </button>
+                </div>
+                <div class="modal-body">
+               
+                      <h5 class="card-title">${data[i].name}</h5>
+                      <p class="card-text">${data[i].address}</p>
+                
+                </div>
+                <div class="modal-footer">
+                  <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                  <button type="button" class="btn btn-primary">Save changes</button>
+                </div>
+              </div>
+            </div>
+          </div>
+
         </div>
       </div>`)
       }
