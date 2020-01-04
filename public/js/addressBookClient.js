a0476049fc2b541cae4f4a65f2b999fc24b4a36b
Oliver Sun <olisun@mac.com>
2020-01-03 20:28:18 -0800

    refactored modal in client js. refactored put method in client js

--- public/js/addressBookClient.js
+++ public/js/addressBookClient.js
@@ -6,6 +6,8 @@ $(document).ready(function () {
   $(document).on("click", "#addData", addData);
   $(document).on("click", "#addData", showData);
   $(document).on("click", ".delete", deleteContact);
+  $(document).on("click", ".updateData", updateData);
+
 
   showData();
 
@@ -21,10 +23,44 @@ $(document).ready(function () {
     });
   }
 
+  function updateData() {
+    event.preventDefault();
+    console.log("inside updateData")
+    console.log("updateData() running")
+    if (!name.val().trim() || !address.val().trim()) {
+      return;
+    }
+    updateContact({
+      name: name.val().trim(),
+      address: address.val().trim()
+    });;
+  }
+
   function newContact(userData) {
     console.log(userData)
     $.post("/api/contacts", userData)
       .then(getContacts)
+      .then(showData);
+  }
+
+  function updateContact(newData) {
+    event.preventDefault();
+    console.log("updateContact() running")
+    console.log("inside updateContact")
+    var id = $(this).data("id");
+    console.log(this)
+    console.log(id)
+    console.log($(this).data())
+    var update = {
+      name: name.val().trim(),
+      address: address.val().trim()
+    }
+    console.log(update)
+    $.ajax({
+      method: "PUT",
+      url: "/api/contacts/" + id,
+      data: update
+    }).then(getContacts)
       .then(showData);
   }
 
@@ -34,47 +70,6 @@ $(document).ready(function () {
       name.val("");
       address.val("");
       console.log(data);
-    });
-  }
-
-  function showData() {
-    console.log("inside showDB")
-    $.get("/api/contacts", function (data) {
-      console.log(data);
-      displayData.empty();
-      for (var i = 0; i < data.length; i++) {
-        displayData.append(`<div class="card" style="width: 18rem;">
-        <div class="card-body">
-          <h5 class="card-title">${data[i].name}</h5>
-          <button class="details btn btn-primary" data-id="${data[i].id}" data-name="${data[i].name}" data-address="${data[i].address}" data-toggle="modal" data-target="#exampleModalCenter" type="button">Details</button>
-          <button class="delete btn btn-primary" data-id="${data[i].id}" data-name="${data[i].name}" data-address="${data[i].address}" type="button">Delete</button>
-
-          <!-- Modal -->
-          <div class="modal fade" id="exampleModalCenter"  role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
-            <div class="modal-dialog modal-dialog-centered" role="document">
-              <div class="modal-content">
-                <div class="modal-header">
-                  <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                  </button>
-                </div>
-                <div class="modal-body">
-               
-                      <h5 class="card-title">${data[i].name}</h5>
-                      <p class="card-text">${data[i].address}</p>
-                
-                </div>
-                <div class="modal-footer">
-                  <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                  <button type="button" class="btn btn-primary">Save changes</button>
-                </div>
-              </div>
-            </div>
-          </div>
-
-        </div>
-      </div>`)
-      }
     });
   }
 
@@ -89,5 +84,51 @@ $(document).ready(function () {
       url: "/api/contacts/" + id
     }).then(getContacts)
       .then(showData);
+  }
+
+  function showData() {
+    console.log("inside showDB")
+    $.get("/api/contacts", function (data) {
+      console.log(data);
+      displayData.empty();
+      for (var i = 0; i < data.length; i++) {
+        displayData.append(
+          `<div class="card" style="width: 18rem;">
+            <div class="card-body">
+              <h5 class="card-title">${data[i].name}</h5>
+              <p class="card-text">${data[i].address}</p>
+              <button class="edit btn btn-primary" data-id="${data[i].id}" data-toggle="modal" data-target="#exampleModal2" type="button">Edit</button>
+              <button class="delete btn btn-primary" data-id="${data[i].id}" type="button">Delete</button>
+            </div>
+          </div>
+          
+          <!-- Modal -->
+          <div class="modal fade" id="exampleModal2" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"   aria-hidden="true">
+            <div class="modal-dialog" role="document">
+              <div class="modal-content">
+                <div class="modal-header">
+                  <h5 class="modal-title" id="exampleModalLabel">Enter Updates</h5>
+                </div>
+                <div class="modal-body">
+                  <form class="updateData">
+                    <div class="form-group">
+                      <label for="name">Name</label>
+                      <input type="text" class="form-control" class="name" aria-describedby="emailHelp">
+                      <small class="form-text text-muted"></small>
+                    </div>
+                  <div class="form-group">
+                    <label for="address">Address</label>
+                    <input type="text" class="form-control" class="address" aria-describedby="emailHelp">
+                    <small class="form-text text-muted"></small>
+                  </div>
+                  <button type="button" data-id="${data[i].id}" class="edit btn btn-primary" data-dismiss="modal">Submit</button>
+                </form>
+                </div>
+              </div>
+            </div>
+          </div>`
+        )
+      }
+    });
   }
 });
